f1ec49191ff9aceb3e558f97387dd18b71a27a9a
nazar-semeriak <nazarsemeriak32@gmail.com>
2025-03-26 14:55:21 +0200

    Added social networks section

--- src/components/auth-forms/form-switcher.tsx
+++ src/components/auth-forms/form-switcher.tsx
@@ -6,13 +6,13 @@ export default function FormSwitcher() {
   const { pathname } = useLocation();
 
   return (
-    <div className="flex flex-1 p-1 rounded-full bg-accent/20 w-full">
+    <div className="flex flex-1 p-1 rounded-full bg-accent/20 min-w-0 w-full sm:max-w-fit">
       <ButtonLink
         href="/auth/login"
         className={clsx(
           pathname.endsWith("/login")
             ? "bg-secondary"
-            : "bg-accent/0 text-primary-foreground"
+            : "bg-accent/0 text-primary-foreground hover:bg-secondary/10"
         )}
       >
         Login
@@ -22,7 +22,7 @@ export default function FormSwitcher() {
         className={clsx(
           pathname.endsWith("/sign-up")
             ? "bg-secondary"
-            : "bg-accent/0 text-primary-foreground"
+            : "bg-accent/0 text-primary-foreground hover:bg-secondary/10"
         )}
       >
         Registration
