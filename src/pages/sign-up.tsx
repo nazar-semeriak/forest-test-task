f4b44280926fa2a431c2c9d203736a06430995fa
nazar-semeriak <nazarsemeriak32@gmail.com>
2025-03-27 22:26:00 +0200

    .

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -39,7 +39,7 @@ export default function SignUp() {
       },
       body: JSON.stringify(data),
     });
-  
+
     if (response.ok) {
       alert("Registration success");
       navigate("/auth/login");
